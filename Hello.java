08fa46356068535330d4562a5c05f980c7625ae9
yuya kinjo <22kinjo_yuuya@axiz.co.jp>
2022-04-22 18:36:56 +0900

    submit

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello { //ブロック
+        //インデント（字下げ）
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(java.time.LocalDateTime.now());
+    }
+}
